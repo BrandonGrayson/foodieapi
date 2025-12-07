14f6aeb5141343c42dc6d4a326ed4a2c53d980ed
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-07 09:31:35 -0500

    Database connection successful

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from typing import Union
 
 from fastapi import FastAPI
+import psycopg
+from psycopg.rows import dict_row
+from psycopg_pool import ConnectionPool
+
 
 app = FastAPI()
+
+try:
+    with psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) as conn:
+        with conn.cursor() as cur:
+            print('database connection successful')
+
+except Exception as e:
+    print('Connection error', e)
+
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
