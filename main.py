31040d9abcaf3e932c4f6d74dc3768e85baa0d51
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-07 14:54:17 -0500

    fetch all users

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from fastapi import FastAPI
 import psycopg
 from psycopg.rows import dict_row
 from psycopg_pool import ConnectionPool
-
+import time
 
 app = FastAPI()
 
-try:
-    with psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) as conn:
-        with conn.cursor() as cur:
-            print('database connection successful')
+while True:
+    try:
+        with psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) as conn:
+            with conn.cursor() as cur:
+                print('database connection successful')
+                break
 
-except Exception as e:
-    print('Connection error', e)
-
-
+    except Exception as e:
+        print('Connection error', e)
+        time.sleep(23)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/users/{username}/{password}")
+def add_user(username: str, password: str):
+    cur.execute(t" INSERT INTO users (username, password) VALUES ({username}), ({password})")
+    return{"users": "User"}
 
