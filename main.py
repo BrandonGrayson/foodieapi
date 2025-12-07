46748c38548da7844b3ba754f43e341e41c6187d
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-07 15:37:19 -0500

    able to add users to database

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ from fastapi import FastAPI
 import psycopg
 from psycopg.rows import dict_row
 from psycopg_pool import ConnectionPool
+from pydantic import BaseModel
 import time
 
 app = FastAPI()
 
+class User(BaseModel):
+    username: str
+    password: str
+
 while True:
     try:
-        with psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) as conn:
-            with conn.cursor() as cur:
-                print('database connection successful')
-                break
+        conn = psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) 
+        cur= conn.cursor() 
+        print('database connection successful')
+        break
 
     except Exception as e:
         print('Connection error', e)
@@ -23,11 +28,13 @@ while True:
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/users/{username}/{password}")
-def add_user(username: str, password: str):
-    cur.execute(t" INSERT INTO users (username, password) VALUES ({username}), ({password})")
-    user = cur.fetchone()
-    return{"users": user}
+@app.post("/users", status_code=201)
+def add_user(user: User):
+    cur.execute("INSERT INTO users (username, password) VALUES (%s, %s) RETURNING *",
+            (user.username, user.password))
+    new_user = cur.fetchone()
+    conn.commit()
+    return{"users": new_user}
 
 @app.get('/users')
 def get_users():
