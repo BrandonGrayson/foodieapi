879ea414ef19ab2ff9152e5a43bb5779cba84a49
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-08 11:28:06 -0500

    route for creating foods

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ class User(BaseModel):
     username: str
     password: str
 
+class Food(BaseModel):
+    name: str
+    description: str
+    location: str
+    grade: int
+    type: str
+
 while True:
     try:
         conn = psycopg.connect(host="localhost", dbname="foodie", user='postgres', password='Prolific1',  row_factory=dict_row) 
@@ -36,10 +43,18 @@ def add_user(user: User):
     conn.commit()
     return{"users": new_user}
 
-@app.get('/users')
+@app.get('/users', status_code=200)
 def get_users():
     cur.execute(t"SELECT * FROM users")
     users = cur.fetchall()
     return {"users": users}
+
+@app.post("/foods", status_code=201)
+def add_food(food: Food):
+    cur.execute("INSERT INTO foods (name, description, location, grade, type) VALUES (%s, %s, %s, %s, %s) RETURNING *",
+                (food.name, food.description, food.location, food.grade, food.type))
+    new_food = cur.fetchone()
+    conn.commit()
+    return{"users": new_food}
 
 
