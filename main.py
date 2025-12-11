fbed7c0285743a18a3f921558a08d0ac17dbfb82
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-11 00:02:15 -0500

    password hashing and updated get users by id route with response mode UserOut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-
+from datetime import datetime, timedelta, timezone
 from fastapi import FastAPI
 import psycopg
 from psycopg.rows import dict_row
@@ -7,7 +7,53 @@ from psycopg_pool import ConnectionPool
 from pydantic import BaseModel
 import time
 from fastapi import HTTPException, status
-from schemas import FoodBase, UserBase, FoodCreate
+from schemas import FoodBase, UserBase, FoodCreate, User, UserOut
+from pwdlib import PasswordHash
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import jwt
+
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+
+password_hash = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+# class UserInDB(User):
+#     hashed_password: str
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# def get_user(db, username: str):
+#     if username in db:
+#         user_dict = db[username]
+#         return UserInDB(**user_dict)
+
+# def authenticate_user(fake_db, username: str, password: str):
+#     user = get_user(fake_db, username)
+#     if not user:
+#         return False
+#     if not verify_password(password, user.hashed_password):
+#         return False
+#     return user
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def hash(password):
+    return password_hash.hash(password)
 
 app = FastAPI()
 
@@ -28,20 +74,22 @@ def read_root():
 
 @app.post("/users", status_code=201)
 def add_user(user: UserBase):
+
+    hashed_password = hash(user.password)
     cur.execute("INSERT INTO users (email, password) VALUES (%s, %s) RETURNING *",
-            (user.email, user.password))
+            (user.email, hashed_password ))
     new_user = cur.fetchone()
 
     conn.commit()
     return new_user
 
-@app.get('/users/{id}', status_code=200)
-def get_users(id: int):
-    cur.execute(t"SELECT * FROM users")
-    users = cur.fetchall()
-    if users == None:
+@app.get('/users/{id}', status_code=200, response_model=UserOut)
+def get_user(id: int):
+    cur.execute("SELECT * FROM users WHERE id = %s", (str(id), ))
+    user = cur.fetchone()
+    if user == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id: {id} was not found")
-    return {"users": users}
+    return user
 
 @app.post("/foods", status_code=201, response_model=FoodCreate)
 def add_food(food: FoodBase):
--- schemas.py
+++ schemas.py
@@ -1,8 +1,20 @@
 from pydantic import BaseModel, EmailStr
+from datetime import datetime
 
 class UserBase(BaseModel):
     email: EmailStr
     password: str
+
+class User(UserBase):
+    hashed_password: str
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
 
 
 class FoodBase(BaseModel):
