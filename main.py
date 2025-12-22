6d1bbd02d7b7a388d88e6c323861191247f95ff1
Brandon Grayson <brandonbgrayson@gmail.com>
2025-12-22 06:51:07 -0500

    route to delete foods by id

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from typing import Annotated
-from datetime import datetime, timedelta, timezone
 from fastapi import FastAPI
-from pydantic import BaseModel
 from fastapi import HTTPException, status
 from models import Users, UserCreate, UserRead, FoodRead, Foods
-from pwdlib import PasswordHash
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-import jwt
+from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends, FastAPI, HTTPException
 from sqlmodel import Session, SQLModel, create_engine, select
 import schemas
-from jwt import PyJWTError
+import oauth
+import utils
 
 app = FastAPI()
 
@@ -28,78 +25,10 @@ def get_session():
 
 SessionDep = Annotated[Session, Depends(get_session)]
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-
-password_hash = PasswordHash.recommended()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-# class UserInDB(User):
-#     hashed_password: str
-
-def create_access_token(data: dict):
-    to_encode = data.copy()
-
-    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-def verify_password(plain_password, hashed_password):
-    return password_hash.verify(plain_password, hashed_password)
-
-
-def hash(password):
-    return password_hash.hash(password)
-
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except InvalidTokenError:
-#         raise credentials_exception
-#     user = get_user(fake_users_db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-def verify_access_token(token: str, credentials_exception):
-
-    try: 
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        id: int = payload.get("user_id")
-
-        if id is None:
-            raise credentials_exception
-    
-        token_data = schemas.TokenData(id=id)
-    except PyJWTError:
-        raise credentials_exception
-    except AssertionError as e:
-        print(e)
-    
-    return token_data.id
-
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth.oauth2_scheme)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    return oauth.verify_access_token(token, credentials_exception)
 
 @app.on_event("startup")
 def on_startup():
@@ -111,9 +40,9 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/users", status_code=201, response_model=UserRead)
-def add_user(user: UserCreate, session: SessionDep):
+def add_user(user: UserCreate, session: SessionDep, user_id: int = Depends(get_current_user)):
 
-    hashed_password = hash(user.password)
+    hashed_password = utils.hash(user.password)
 
     db_user = Users(email=user.email, password=hashed_password)
 
@@ -124,7 +53,7 @@ def add_user(user: UserCreate, session: SessionDep):
     return db_user
 
 @app.get('/users/{id}', status_code=200, response_model=UserRead)
-def get_user(id: int, session: SessionDep):
+def get_user(id: int, session: SessionDep, user_id: int = Depends(get_current_user)):
     
     statement = select(Users).where(Users.id == id)
     user = session.exec(statement).first()
@@ -149,7 +78,7 @@ def add_food(food: schemas.FoodCreate, session: SessionDep, user_id: int = Depen
     return db_Foods
 
 @app.get('/foods/{id}', status_code=200, response_model=FoodRead)
-def get_foods(id: int, session: SessionDep):
+def get_foods(id: int, session: SessionDep, user_id: int = Depends(get_current_user)):
 
     statement = select(Foods).where(Foods.id == id)
     food = session.exec(statement).first()
@@ -159,7 +88,19 @@ def get_foods(id: int, session: SessionDep):
 
     return food
 
-@app.post("/login")
+@app.delete("/foods/{id}", response_model=schemas.FoodCreate)
+def delete_food(id: int, session: SessionDep, user_id: int = Depends(get_current_user)):
+
+    statement = select(Foods).where(Foods.id == id)
+    food = session.exec(statement).one()
+
+    session.delete(food)
+    session.commit()
+
+    return food
+
+
+@app.post("/login", response_model=schemas.Token)
 def login(user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()], session: SessionDep):
 
    statement = select(Users).where(Users.email == user_credentials.username)
@@ -168,9 +109,12 @@ def login(user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()], ses
    if not user:
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
    
-   if not verify_password(user_credentials.password, user.password):
+   if not utils.verify_password(user_credentials.password, user.password):
        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
    
-   access_token = create_access_token(data = {"user_id": user.id})
+   access_token = oauth.create_access_token(data = {"user_id": user.id})
    
    return {"access_token": access_token, "token_type": "bearer"}
+
+
+
---
+++ oauth.py
@@ -0,0 +1,40 @@
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from datetime import datetime, timedelta, timezone
+import jwt
+import schemas
+from jwt import PyJWTError
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+
+    try: 
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: int = payload.get("user_id")
+
+        if id is None:
+            raise credentials_exception
+    
+        token_data = schemas.TokenData(id=id)
+    except PyJWTError:
+        raise credentials_exception
+    except AssertionError as e:
+        print(e)
+    
+    return token_data.id
